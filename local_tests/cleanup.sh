f0a6acb5ad9d6e0cb9012529cf45f7ca443c72b4
Andrea Garbato <andrea.garbato@lastminute.com>
2021-01-27 19:26:41 +0100

    cleanup script

--- local_tests/cleanup.sh
+++ local_tests/cleanup.sh
@@ -5,3 +5,4 @@ rm -rf client server
 mkdir -p client/share
 echo "Test file client" > client/share/client_test.txt
 mkdir -p server/shares
+touch client/.gitkeep client/share/.gitkeep server/.gitkeep server/shares/.gitkeep
