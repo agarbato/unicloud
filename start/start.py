92f27a93390f294e6a2efc07de7d528754e49820
Andrea Garbato <andrea.garbato@lastminute.com>
2021-01-29 10:07:48 +0100

    ssh git fix permission

--- start/start.py
+++ start/start.py
@@ -81,7 +81,7 @@ def gen_key():
     ShellCmd("ssh-keygen -A ; mv /etc/ssh /data/")
     # fix possible wrong permission on home folder that prevents ssh to work properly
     ShellCmd("chmod g-w /data")
-else:
+  else:
     #print (ShellCmd("ls -al /data/"))
     cmd = ShellCmd("su -c \"ssh-keygen -f /data/.ssh/id_rsa -t rsa -N '' \" %s" % user)
     print (cmd)
