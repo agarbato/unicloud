e908f67c015f1f9433cf10b3f7be941c7d73332a
Andrea Garbato <andrea.garbato@lastminute.com>
2021-08-24 16:40:11 +0200

    Code cleanup

--- app_client/log.py
+++ app_client/log.py
@@ -1,25 +1,32 @@
 from time import sleep, strftime
 from conf import *
 
+
 class Log(object):
    filename = ""
    format = "%Y/%m/%d %H:%M:%S"
-   def __init__(self,logfile):
+
+   def __init__(self, logfile):
      self.logfile = logfile
-     self.filename = open(logfile,"a")
+     self.filename = open(logfile, "a")
+
    def header(self):
      self.filename.write("\n======================================================================================")
+
    def sync_start(self):
-     self.filename.write("\n%s Unicloud Sync Started\n" % strftime(self.format))
-   def sync_end(self,result):
+     self.filename.write(f"\n{strftime(self.format)} Unicloud Sync Started\n")
+
+   def sync_end(self, result):
      self.result = result
-     self.filename.write("\n%s" % result[3])
-     self.filename.write("\n%s Unicloud Sync %s, Pid %d, Exitcode %d" % (strftime(self.format), result[2], int(result[0]), int(result[1])))
-   def client_error(self,result):
+     self.filename.write(f"\n{result[3]}")
+     self.filename.write(f"\n{strftime(self.format)} Unicloud Sync {result[2]}, Pid {int(result[0])}, Exitcode {int(result[1])}")
+
+   def client_error(self, result):
      self.result = result
-     self.filename.write("\n%s %s" % ( strftime(self.format), result ))
+     self.filename.write(f"\n{strftime(self.format)} {result}")
      self.header()
      self.close()
+
    def close(self):
      self.filename.close()
 
--- app_client/shell.py
+++ app_client/shell.py
@@ -1,44 +1,47 @@
 import subprocess
 
+
 class ShellCmd(object):
-    output={}
-    output_stderr=[]
-    output_lines=[]
-    rc=0
-    pid=0
-    def __init__(self,cmd):
-      self.cmd  = cmd
-      out = subprocess.Popen([cmd],
-        shell=True,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE)
-      stdout,stderr = out.communicate()
-      self.output = stdout.decode()[:-1]
-      self.rc=out.returncode
-      self.pid=out.pid
-      if stderr is not None:
-        self.output_stderr = stderr.decode()[:-1]
-      #print (self.output)
-      for line in self.output.split('\n'):
-        self.output_lines.append(line)
+    output = {}
+    output_stderr = []
+    output_lines = []
+    rc = 0
+    pid = 0
+
+    def __init__(self, cmd):
+        self.cmd = cmd
+        out = subprocess.Popen([cmd],
+                               shell=True,
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE)
+        stdout, stderr = out.communicate()
+        self.output = stdout.decode()[:-1]
+        self.rc = out.returncode
+        self.pid = out.pid
+        if stderr is not None:
+            self.output_stderr = stderr.decode()[:-1]
+        # print (self.output)
+        for line in self.output.split('\n'):
+            self.output_lines.append(line)
+
     def __repr__(self):
-      return self.output
+        return self.output
+
     def rstderr(self):
-      return self.output_stderr
+        return self.output_stderr
+
     def getrc(self):
-      return self.rc
+        return self.rc
+
     def getpid(self):
-      return self.pid
-    def filter_lines(self,match):
-        self.filter=[]
-        self.match=match
-        i=1
+        return self.pid
+
+    def filter_lines(self, match):
+        self.filter = []
+        self.match = match
+        i = 1
         while i != len(self.output_lines):
             if any(x in str(self.output_lines[i]) for x in self.match):
                 self.filter.append(self.output_lines[i])
             i += 1
         return self.filter
-
-
-
-
